9425e7a61c64e2123a9208844dd23d1c362cd3a2
Francesco Paolo Lovergine <frankie@debian.org>
2023-12-28 16:11:16 +0100

    Now also installing docbooks.

--- src/time_mono.h
+++ src/time_mono.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define TIME_MONO_H
 
 #include <time.h>
+#include <sys/types.h>
 
 /*INITIALIZE*/
 extern void time_mono_init(void);
